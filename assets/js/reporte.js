3eb8a2d6867a13d5150c3fefcb0b1e91b99b9998
JaimePalacios13 <jaimepalacios998@gmail.com>
2022-06-10 09:31:32 -0600

    Final

--- assets/js/reporte.js
+++ assets/js/reporte.js
@@ -1,73 +1,72 @@
-$(document).ready(function () {
-  $("#table-entradas").DataTable();
-  $("#table-salidas").DataTable();
+$(document).ready(function() {
+    $("#table-entradas").DataTable();
+    $("#table-salidas").DataTable();
 
-  refresh_datatables();
+    refresh_datatables();
 });
 
 function refresh_datatables() {
-  fecha_inicio = $("#fecha_inicio").val();
+    fecha_inicio = $("#fecha_inicio").val();
 
-  data = { fecha_inicio };
+    data = { fecha_inicio };
 
-  console.log(data);
+    console.log(data);
 
-  $.ajax({
-    type: "get",
-    url: baseURL + "reportes/data/all",
-    data: {fecha_inicio},
-    dataType: "json",
-    success: function (response) {
-      $("#table-entradas").dataTable().fnDestroy();
-      $("#table-salidas").dataTable().fnDestroy();
+    $.ajax({
+        type: "get",
+        url: baseURL + "reportes/data/all",
+        data: { fecha_inicio },
+        dataType: "json",
+        success: function(response) {
+            $("#table-entradas").dataTable().fnDestroy();
+            $("#table-salidas").dataTable().fnDestroy();
 
-      var data_entry = ` `;
-      var data_out = ` `;
-      var total_salidas = 0;
-      var total_entradas = 0;
+            var data_entry = ` `;
+            var data_out = ` `;
+            var total_salidas = 0;
+            var total_entradas = 0;
 
-      if (response != "no_register") {
-        for (let i = 0; i < response.entradas.length; i++) {
-          data_entry += ` 
+            if (response != "no_register") {
+                for (let i = 0; i < response.entradas.length; i++) {
+                    data_entry += ` 
                  <tr>
                       <td>${response.entradas[i]["tipoEntrada"]}</td>
                       <td>$${response.entradas[i]["monto"]}</td>
                   </tr>`;
 
-          total_entradas += parseFloat(response.entradas[i]["monto"]);
-        }
+                    total_entradas += parseFloat(response.entradas[i]["monto"]);
+                }
 
-        for (let i = 0; i < response.salidas.length; i++) {
-          data_out += ` 
+                for (let i = 0; i < response.salidas.length; i++) {
+                    data_out += ` 
            <tr>
                 <td>${response.salidas[i]["tipoSalida"]}</td>
                 <td>$${response.salidas[i]["monto"]}</td>
             </tr>`;
-          total_salidas += parseFloat(response.salidas[i]["monto"]);
-        }
-      }
+                    total_salidas += parseFloat(response.salidas[i]["monto"]);
+                }
+            }
+            create_graq(total_entradas, total_salidas);
 
-      create_graq(total_entradas, total_salidas);
+            document.querySelector("#data-entradas").textContent = "";
+            document.querySelector("#data-entradas").innerHTML = data_entry;
 
-      document.querySelector("#data-entradas").textContent = "";
-      document.querySelector("#data-entradas").innerHTML = data_entry;
+            document.querySelector("#data-salidas").textContent = "";
+            document.querySelector("#data-salidas").innerHTML = data_out;
 
-      document.querySelector("#data-salidas").textContent = "";
-      document.querySelector("#data-salidas").innerHTML = data_out;
+            $("#table-entradas").DataTable({
+                language: {
+                    url: "https://cdn.datatables.net/plug-ins/1.11.5/i18n/es-MX.json",
+                },
+            });
 
-      $("#table-entradas").DataTable({
-        language: {
-          url: "https://cdn.datatables.net/plug-ins/1.11.5/i18n/es-MX.json",
+            $("#table-salidas").DataTable({
+                language: {
+                    url: "https://cdn.datatables.net/plug-ins/1.11.5/i18n/es-MX.json",
+                },
+            });
         },
-      });
-
-      $("#table-salidas").DataTable({
-        language: {
-          url: "https://cdn.datatables.net/plug-ins/1.11.5/i18n/es-MX.json",
-        },
-      });
-    },
-  });
+    });
 }
 
 /* Evento para hacer la petición cada vez que el usuario seleccione una nueva fecha */
@@ -76,53 +75,63 @@ var ctx = document.getElementById("chart1");
 
 
 function create_graq(total_entradas, total_salidas) {
- /*  $("#chart1").remove();  
-  */ 
- 
- 
- var data = {
-    labels: ["Entradas", "Salidas"],
-    datasets: [
-      {
-        label: "# Balance",
-        data: [total_entradas, total_salidas],
-        backgroundColor: ["rgba(15, 35, 201, 1)", "rgba(231, 15, 64, 1)"],
-        borderColor: ["rgba(200,200,200,1)", "rgba(200,200,200,1)"],
-        borderWidth: 2,
-      },
-    ],
-  };
-  var options = {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true,
-          },
+    /*  $("#chart1").remove();  
+     */
+    var data = {
+        labels: ["Entradas", "Salidas"],
+        datasets: [{
+            label: "# Balance",
+            data: [total_entradas, total_salidas],
+            backgroundColor: ["rgba(15, 35, 201, 1)", "rgba(231, 15, 64, 1)"],
+            borderColor: ["rgba(200,200,200,1)", "rgba(200,200,200,1)"],
+            borderWidth: 2,
+        }, ],
+    };
+    var options = {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                },
+            }, ],
         },
-      ],
-    },
-  };
-/*   document.querySelector("#contiene_chart").innerHTML = '<canvas id="chart1" width="400" height="100"></canvas>';
- */
-  var chart1 = new Chart(ctx, {
-    type: "doughnut",
-    data: data,
-    options: options,
-  });
+    };
+    /*   document.querySelector("#contiene_chart").innerHTML = '<canvas id="chart1" width="400" height="100"></canvas>';
+     */
+    var chart1 = new Chart(ctx, {
+        type: "doughnut",
+        data: data,
+        options: options,
+    });
 
-  document.querySelector("#t_entradas").textContent = "$"+total_entradas;
-  document.querySelector("#t_salidas").textContent = "$"+total_salidas;
+    document.querySelector("#t_entradas").textContent = "$" + total_entradas;
+    document.querySelector("#t_salidas").textContent = "$" + total_salidas;
 
-  tt = parseFloat(total_entradas) + parseFloat(total_salidas);
+    tt = parseFloat(total_entradas) + parseFloat(total_salidas);
 
-  document.querySelector("#porcentaje_entradas").textContent = "Entradas: "+(parseFloat(total_entradas * 100) / tt).toFixed(2) + "%";
-  document.querySelector("#porcentaje_salidas").textContent = "Salidas: "+(parseFloat(total_salidas * 100) / tt).toFixed(2) + "%";
- 
+    tt_entradas = (parseFloat(total_entradas * 100) / tt).toFixed(2);
+    tt_salidas = (parseFloat(total_salidas * 100) / tt).toFixed(2)
+    if (isNaN(tt_entradas)) {
+        $('#contiene_chart').css('display', 'none')
+        tt_entradas = '0'
+    } else {
+        $('#contiene_chart').css('display', 'block')
+        tt_entradas = tt_entradas
+    }
+    if (isNaN(tt_salidas)) {
+        $('#contiene_chart').css('display', 'none')
+        tt_salidas = '0'
+    } else {
+        $('#contiene_chart').css('display', 'block')
+        tt_salidas = tt_salidas
+    }
+    document.querySelector("#porcentaje_entradas").textContent = "Entradas: " + tt_entradas + "%";
+    document.querySelector("#porcentaje_salidas").textContent = "Salidas: " + tt_salidas + "%";
+
 }
 
 document.querySelector("#fecha_inicio").addEventListener("input", () => {
-  refresh_datatables();
+    refresh_datatables();
 });
 
 
